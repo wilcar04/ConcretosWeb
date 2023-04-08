606eaa83be70c19eeea8a9a6fb00fbb7c8c74d4c
Daniel315a <daniel315.a@gmail.com>
2023-04-08 15:33:01 -0500

    Se agrega código para ejecutar las migraciones automáticamente

--- Concretos/Program.cs
+++ Concretos/Program.cs
@@ -8,13 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+));
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option => {
     option.LoginPath = "/Access/Login";
     option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) { 
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
